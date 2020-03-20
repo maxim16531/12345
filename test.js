b07df68b2af374c24ac9bb73a2f57b0b45eae93d
maxim16531 <maximkoroteev2016@yandex.ru>
2020-03-20 17:23:58 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,17 @@
+class Shop {
+    constructor(name) {
+        this.name = name;
+        this.count = 0;
+    }
+   // products() }
+    if(onclick){
+        this.count++;
+        console.log(this.count);
+    }
+    else() {
+        console.log("Ошибка")
+    }
+     //   this.count ++;  //   alert(this.name + "Осталось столько - "  + this.count);
+};
+//var s = new Shop("apple");
+//s.products();
